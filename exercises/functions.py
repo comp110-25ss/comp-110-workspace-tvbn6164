29e7b2be85a8784d493e9ba2c23db270019c0c60
Thomas Nguyen <tvbn@unc.edu>
2025-06-26 11:17:38 -0400

    Finished Exercise 00!

---
+++ exercises/functions.py
@@ -0,0 +1,9 @@
+"Practice writing and calling functions,"
+
+
+def sum(num1: int, num2: int) -> int:  # <- signature
+    "Add two numbres together."
+    return num1 + num2
+
+
+print(sum(num1=11, num2=3))
---
+++ exercises/memory_diagrams.py
@@ -0,0 +1,5 @@
+def total_cost(price: int, tax_rate: float) -> float:
+    print(price + (price * tax_rate))
+
+
+print(total_cost(price=100, tax_rate=0.07))
